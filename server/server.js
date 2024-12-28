bf80f8039c053a13d7475e60ba0b200ef27e1bad
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-28 15:34:43 +0200

    add backend logic

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require("uuid");
+const data = require('./db.json');
+
+const PORT = 3001;
+
+const app = express();
+
+const corsOptions = {
+  origin: ["http://localhost:8080", "http://localhost:3001"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+
+app.get('/destination', (req, res) => {
+    res.json(data.destination);
+});
+
+app.get('/hotels', (req, res) => {
+    res.json(data.hotels);
+});
+
+app.post('/hotels', (req, res) => {
+    const { city } = req.body;
+    const hotels = data.hotels.filter((hotel) => hotel.city === city);
+    res.json(hotels);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+  });
