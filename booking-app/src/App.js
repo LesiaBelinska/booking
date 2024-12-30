1ffd06e6aff068c61b1b5cf90e1ac2ed8137a906
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-31 01:10:21 +0200

    Update App.js

--- booking-app/src/App.js
+++ booking-app/src/App.js
@@ -1,8 +1,30 @@
+import { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Layout from "./components/Layout/Layout.jsx";
+import Loader from "./components/Loader/Loader.jsx";
+
+const MainPage = lazy(() => import("./pages/MainPage/MainPage.jsx"));
+const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
+const AboutPage = lazy(() => import("./pages/AboutPage/AboutPage.jsx"));
+const HotelsPage = lazy(() => import("./pages/HotelsPage/HotelsPage.jsx"));
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage.jsx"));
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<MainPage />} />
+            <Route path="hotels" element={<HotelsPage />} />
+            <Route path="about" element={<AboutPage />} />
+            <Route path="home" element={<HomePage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </>
   );
 }
 
