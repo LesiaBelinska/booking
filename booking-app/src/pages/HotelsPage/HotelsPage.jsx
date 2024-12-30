43a19db72414933b0ea56c6101a620fa354662a8
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-31 01:10:58 +0200

    Create HotelsPage.jsx

---
+++ booking-app/src/pages/HotelsPage/HotelsPage.jsx
@@ -0,0 +1,11 @@
+import s from "./HotelsPage.module.css";
+
+const HotelsPage = () => {
+  return (
+    <div className={s.container}>
+      Hotels Page
+    </div>
+  )
+};
+
+export default HotelsPage;
