086c2b1d4d1c271153d79f3216c44ffb191e813a
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2024-12-31 01:10:23 +0200

    Create AppBar.jsx

---
+++ booking-app/src/components/AppBar/AppBar.jsx
@@ -0,0 +1,13 @@
+import Navigation from "../Navigation/Navigation.jsx";
+import s from "./AppBar.module.css";
+
+const AppBar = () => {
+  
+  return (
+    <header className={s.header}>
+      <Navigation />
+    </header>
+  );
+};
+
+export default AppBar;
