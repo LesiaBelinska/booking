1d118e7b967c453eb4f38e277e628fa95cb30237
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2025-01-02 14:07:06 +0200

    Update Navigation.jsx

--- booking-app/src/components/Navigation/Navigation.jsx
+++ booking-app/src/components/Navigation/Navigation.jsx
@@ -1,42 +1,46 @@
 import { NavLink } from "react-router-dom";
 
+import Logo from "../Logo/Logo.jsx";
 import s from "./Navigation.module.css";
 
 const Navigation = () => {
-
   return (
     <nav>
       <ul className={s.list}>
-        <li className={s.item}>
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive ? `${s.activeLink}` : `${s.link}`
-            }
-          >
-            Booking
-          </NavLink>
-        </li>
-        <li className={s.item}>
-          <NavLink
-            to="/home"
-            className={({ isActive }) =>
-              isActive ? `${s.activeLink}` : `${s.link}`
-            }
-          >
-            Home
-          </NavLink>
-        </li>
-        <li className={s.item}>
-          <NavLink
-            to="/about"
-            className={({ isActive }) =>
-              isActive ? `${s.activeLink}` : `${s.link}`
-            }
-          >
-            About
-          </NavLink>
-        </li>
+        <div className={s.logoWrapper}>
+          <li className={s.item}>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? `${s.activeLink}` : `${s.link}`
+              }
+            >
+              <Logo />
+            </NavLink>
+          </li>
+        </div>
+        <div className={s.navigationWrapper}>
+          <li className={s.item}>
+            <NavLink
+              to="/home"
+              className={({ isActive }) =>
+                isActive ? `${s.activeLink}` : `${s.link}`
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className={s.item}>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive ? `${s.activeLink}` : `${s.link}`
+              }
+            >
+              About
+            </NavLink>
+          </li>
+        </div>
       </ul>
     </nav>
   );
