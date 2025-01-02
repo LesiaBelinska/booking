765b3d3fce24bef893cd01a3e553aae5b23406f5
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2025-01-02 18:01:42 +0200

    Update index.js

--- booking-app/src/index.js
+++ booking-app/src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
-import store from "./redux/store.js";
+import store, { persistor } from "./redux/store.js";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -12,7 +13,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
