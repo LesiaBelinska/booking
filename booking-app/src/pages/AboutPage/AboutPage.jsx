9e383cbf54e3ebe44aadb246f57c5dd64251d780
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2025-01-02 15:05:14 +0200

    Update AboutPage.jsx

--- booking-app/src/pages/AboutPage/AboutPage.jsx
+++ booking-app/src/pages/AboutPage/AboutPage.jsx
@@ -1,9 +1,18 @@
 import s from "./AboutPage.module.css";
 
 const AboutPage = () => {
-    return (
-        <div className={s.container}>About Page</div>
-    )
-}
+  return (
+    <div className={s.container}>
+      <h3 className={s.title}>About</h3>
+      <p className={s.text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit molestiae
+        suscipit autem explicabo aperiam ipsum, tenetur facere sequi ab sint
+        dicta asperiores possimus totam enim aut error consequatur repudiandae
+        blanditiis corporis dolores minus qui reprehenderit modi magnam! Nam,
+        molestias a. Autem esse neque eos quod.
+      </p>
+    </div>
+  );
+};
 
 export default AboutPage;
