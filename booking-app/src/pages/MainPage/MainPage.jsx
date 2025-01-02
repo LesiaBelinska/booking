f74ab612a9cedf0471ca52b35b0052da584201ef
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2025-01-02 16:39:42 +0200

    Update MainPage.jsx

--- booking-app/src/pages/MainPage/MainPage.jsx
+++ booking-app/src/pages/MainPage/MainPage.jsx
@@ -1,15 +1,37 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getDestinations, searchHotels } from "../../redux/bookingThunk.js";
+
 import SearchForm from "../../components/SearchForm/SearchForm.jsx";
+import HotelList from "../../components/HotelList/HotelList.jsx";
+
 import s from "./MainPage.module.css";
 
 const MainPage = () => {
+
+  const dispatch = useDispatch();
+  const { destinations, hotels, loading, error } = useSelector(
+    (state) => state.booking
+  );
+
+  useEffect(() => {
+    dispatch(getDestinations());
+  }, [dispatch]);
+
+  const handleSearch = (values) => {
+    dispatch(searchHotels(values.destination));
+  };
+
   return (
-    <>
-      <SearchForm />
-      <div className={s.container}>
-        Main Page
-      </div>
-    </>
-  )
+    <div className={s.pageWrapper}>
+      <SearchForm onSubmit={handleSearch} destinations={destinations} />
+
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+
+      {!loading && !error && hotels.length > 0 && <HotelList />}
+    </div>
+  );
 };
 
 export default MainPage;
