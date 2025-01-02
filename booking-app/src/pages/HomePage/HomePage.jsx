9b7ea4fc91b886c2b20a841c9feb6ecd223118d5
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2025-01-02 15:05:19 +0200

    Update HomePage.jsx

--- booking-app/src/pages/HomePage/HomePage.jsx
+++ booking-app/src/pages/HomePage/HomePage.jsx
@@ -2,8 +2,17 @@ import s from "./HomePage.module.css";
 
 const HomePage = () => {
   return (
-    <div className={s.container}>Home Page</div>
-  )
+    <div className={s.container}>
+      <h3 className={s.title}>Home</h3>
+      <p className={s.text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit molestiae
+        suscipit autem explicabo aperiam ipsum, tenetur facere sequi ab sint
+        dicta asperiores possimus totam enim aut error consequatur repudiandae
+        blanditiis corporis dolores minus qui reprehenderit modi magnam! Nam,
+        molestias a. Autem esse neque eos quod.
+      </p>
+    </div>
+  );
 };
 
 export default HomePage;
