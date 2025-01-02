795011493a892ca7dffa7a5acc7202c1a8a75817
Lesia Belinska <79412066+LesiaBelinska@users.noreply.github.com>
2025-01-02 18:01:57 +0200

    Update store.js

--- booking-app/src/redux/store.js
+++ booking-app/src/redux/store.js
@@ -1,10 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+
 import bookingReducer from "./bookingSlice.js";
+
+const persistConfig = {
+  key: "booking",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, bookingReducer);
 
 const store = configureStore({
   reducer: {
-    booking: bookingReducer,
+    booking: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
 });
 
+export const persistor = persistStore(store);
 export default store;
